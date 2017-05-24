8eab36002a744ec7dbaff9ad42783698f7abc999
Anudle <AnuShrestha007@gmail.com>
2017-05-23 22:20:54 -0600

    init and auth

---
+++ src/components/auth/auth.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div id='auth'>
+    <div class="container">
+          <div class="row ">
+              <div class="col-md-4 col-md-offset-4 authForm">
+                <h2 class="text-center">Beeper</h2>
+                <router-view></router-view>
+              </div>
+            </div>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'auth'
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Auth from './components/auth/auth.vue';
+import Login from './components/auth/login.vue'
+import Register from './components/auth/register.vue'
+
+Vue.use(VueRouter);
+
+var router = new VueRouter({
+  routes: [
+    {
+      path: '/auth',
+      component: Auth,
+      redirect: '/auth/login'
+      children: [
+        {
+          path: 'login',
+          component: Login
+        },
+        {
+          path: 'register',
+          component: Register
+        }
+      ]
+
+
+    }
+  ]
+});
+
+export default router;
