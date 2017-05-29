25882aad5b2e022ee178db189359e8b77b254a48
Anudle <AnuShrestha007@gmail.com>
2017-05-28 20:23:16 -0600

    alertify

--- src/components/auth/register.vue
+++ src/components/auth/register.vue
@@ -1,15 +1,11 @@
 <template lang="html">
   <div>
       <h3 class="text-center">Register</h3>
-
-      <input type="email" class="form-control m-b-15" placeholder="Email address">
-      <input type="text" class="form-control m-b-15" placeholder="Username">
-      <input type="password" class="form-control m-b-15" placeholder="Password">
-
+      <input type="email" class="form-control m-b-15" placeholder="Email address" v-model="user.email">
+      <input type="text" class="form-control m-b-15" placeholder="Username" v-model="user.username">
+      <input type="password" class="form-control m-b-15" placeholder="Password" v-model="user.password">
       <hr>
-
-      <button class="btn btn-lg btn-primary btn-block m-b-15">Register</button>
-
+      <button class="btn btn-lg btn-primary btn-block m-b-15" @click='register'>Register</button>
       <p class="text-center">
           Already have an account? <router-link to="/auth/login">Login!</router-link>
       </p>
@@ -18,7 +14,31 @@
 
 <script>
 export default {
-  name: 'register'
+  name: 'register',
+  data: function(){
+    return {
+      user: {
+        email: "",
+        username: "",
+        password: "",
+      }
+    }
+  },
+  methods: {
+    register(){
+      this.$http.post("http://localhost:9090/users", this.user)
+        .then(function(res){
+          alertify.success("Success! You can now login with your email and password");
+          this.$router.push('/auth/login');
+        }).catch(function(res){
+          if(res.status == 422){
+            res.body.errors.forEach(function (e) {
+              alertify.error(e)
+            })
+          }
+        })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import Router from './routes.js'
+import Vue from 'vue';
+import App from './App';
+import Router from './routes.js';
+import VueResource from 'vue-resource';
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+
+//configure alertify defaults
+
 
 /* eslint-disable no-new */
 new Vue({
